6fb1878f2fec3beab8eede4eac88025113644854
nates321 <streln@rpi.edu>
2017-03-09 16:35:31 -0500

    shows correct amount of representatives in house for  each state

--- public/js/people.js
+++ public/js/people.js
@@ -1,7 +1,10 @@
 // Map from https://github.com/cody1213/twism
 
 // Dictionary of state abbrevisations to names
-var states = {'AZ': 'Arizona','AL': 'Alabama','AK': 'Alaska','AZ': 'Arizona','AR': 'Arkansas','CA': 'California','CO': 'Colorado','CT': 'Connecticut','DE': 'Delaware','FL': 'Florida','GA': 'Georgia','HI': 'Hawaii','ID': 'Idaho','IL': 'Illinois','IN': 'Indiana','IA': 'Iowa','KS': 'Kansas','KY': 'Kentucky','KY': 'Kentucky','LA': 'Louisiana','ME': 'Maine','MD': 'Maryland','MA': 'Massachusetts','MI': 'Michigan','MN': 'Minnesota','MS': 'Mississippi','MO': 'Missouri','MT': 'Montana','NE': 'Nebraska','NV': 'Nevada','NH': 'New Hampshire','NJ': 'New Jersey','NM': 'New Mexico','NY': 'New York','NC': 'North Carolina','ND': 'North Dakota','OH': 'Ohio','OK': 'Oklahoma','OR': 'Oregon','PA': 'Pennsylvania','RI': 'Rhode Island','SC': 'South Carolina','SD': 'South Dakota','TN': 'Tennessee','TX': 'Texas','UT': 'Utah','VT': 'Vermont','VA': 'Virginia','WA': 'Washington','WV': 'West Virginia','WI': 'Wisconsin','WY': 'Wyoming'};
+var states = {'AL': 'Alabama','AK': 'Alaska','AZ': 'Arizona','AR': 'Arkansas','CA': 'California','CO': 'Colorado','CT': 'Connecticut','DE': 'Delaware','FL': 'Florida','GA': 'Georgia','HI': 'Hawaii','ID': 'Idaho','IL': 'Illinois','IN': 'Indiana','IA': 'Iowa','KS': 'Kansas','KY': 'Kentucky','KY': 'Kentucky','LA': 'Louisiana','ME': 'Maine','MD': 'Maryland','MA': 'Massachusetts','MI': 'Michigan','MN': 'Minnesota','MS': 'Mississippi','MO': 'Missouri','MT': 'Montana','NE': 'Nebraska','NV': 'Nevada','NH': 'New Hampshire','NJ': 'New Jersey','NM': 'New Mexico','NY': 'New York','NC': 'North Carolina','ND': 'North Dakota','OH': 'Ohio','OK': 'Oklahoma','OR': 'Oregon','PA': 'Pennsylvania','RI': 'Rhode Island','SC': 'South Carolina','SD': 'South Dakota','TN': 'Tennessee','TX': 'Texas','UT': 'Utah','VT': 'Vermont','VA': 'Virginia','WA': 'Washington','WV': 'West Virginia','WI': 'Wisconsin','WY': 'Wyoming'};
+
+// how many house representatives there are
+var representatives = {'AL' : 7,'AK': 1, 'AZ': 9, 'AR': 4, 'CA': 53,'CO': 7,'CT': 5, 'DE': 1,'FL': 27, 'GA' : 14, 'HI' : 2, 'ID': 2, 'IL' : 18, 'IN': 9, 'IA' : 4, 'KS' : 4, 'KY' : 6, 'LA' : 6, 'ME': 2, 'MD': 8, 'MA': 9, 'MI': 14,'MN' : 8,'MS' : 4, 'MO' : 8, "MT" : 1, 'NE': 3, 'NV':  4,'NH': 2, 'NJ': 12, 'NM': 3,'NY' : 27, 'NC' : 13, 'ND' :1, 'OH': 16,'OK' : 5, 'OR' : 5, 'PA' : 18,'RI': 2,'SC' : 7,'SD': 1, 'TN': 9, 'TX': 36, 'UT' : 4, 'VT' : 1, 'VA' : 11, 'WA': 10, 'WV' : 3, 'WI': 8, 'WY': 1};
 
 // senate or house
 var choice = "";
@@ -25,7 +28,20 @@ function loadPeople(state, choice) {
     'state': state,
     'site': '#'
   });
-  // end phony data
+  if (choice != "senate"){
+      for (var i = 3; i <= representatives[state]; i++){
+
+        data.push({
+          'img': 'http://placehold.it/350',
+          'name':  "Representative  "+ i ,
+          'state': state,
+          'site': '#'
+        });
+
+
+      }
+      // end phony data
+  }
 
   for (var i in data) {
     person = data[i];
