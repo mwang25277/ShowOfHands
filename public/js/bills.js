d22e30667f6934e0f690fe0852f98b827e1a0599
nates321 <streln@rpi.edu>
2017-04-14 17:50:00 -0400

    displays bills

--- public/js/bills.js
+++ public/js/bills.js
@@ -4,6 +4,60 @@
    app.controller('billsCtrl', function($scope, $http, $window) {
       //used to display bills onto page
       $scope.x = 5;
+      $scope.bills = [];
+      $scope.view= 1;
+
+
+      $scope.class = [];
+      $scope.class2 = [];
+  	  $scope.changeClass = function(){
+    	/*if ($scope.class === "active"){
+      		$scope.class.pop('active');
+      		$scope.class2.push('active');
+    	}
+    	else if ($scope.class === "active"){
+      		$scope.class.pop('active');
+      		$scope.class2.push('active');
+    	}
+    	else if ($scope.class === "active"){
+      		$scope.class.pop('active');
+      		$scope.class2.push('active');
+    	}
+    	else if ($scope.class === "active"){
+      		$scope.class.pop('active');
+      		$scope.class2.push('active');
+    	}
+    	else {
+      		$scope.class.pop('active');
+      		$scope.class2.push('active');
+      	}*/
+  	  };
+
+
+      $scope.getBills = function(){
+      	
+      	$http({
+      		url: '/bills',
+      		method: 'GET'
+      	}).then(
+      		function successCallback(response){
+      			$scope.bills = response.data;
+
+      		},
+      		function errorCallback(response){
+
+      			console.log(response);
+      		}
+
+
+      	);
+
+
+      };
+      
+
+
+
    });
 
 })(window.angular);
