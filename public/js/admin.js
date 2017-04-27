1c1d72b80d2e692c0b351eb7525e4510679a534f
Max Wang <wangm13@rpi.edu>
2017-04-27 13:52:28 -0400

    fixed syntax for admin

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,11 +1,13 @@
 (function(angular) {
    'use strict';
-   var app = angular.module('adminApp', ['nvd3']);
+   var app = angular.module('adminApp', []);
    app.controller('adminCtrl', function($scope, $http, $window) {
-    $scope.updateDB = function() {
-      $.post('/update-db', function(data, status) {
-        console.log(data);
+      $scope.updateDB = function() {
+        console.log("Update...");
+        $.post('/update-db', function(data, status) {
+          console.log(data);
+        });
+      };
     });
-    }
 
 })(window.angular);
